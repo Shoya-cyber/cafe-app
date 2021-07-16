95288754bf72814de88e5c876c73ab18cb75c20a
Shoya-cyber <shoyasan0627@gmail.com>
2021-07-16 23:24:36 +0900

    postコントローラーにindex,createアクションとパラメーター記述

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,31 @@
 class PostsController < ApplicationController
 
-def index
-end
+  def index
+    @posts = Post.all.order("created_at DESC")
+  end
 
-def new 
-  @post = Post.new
-end
+  def new 
+    @post = Post.new
+  end
 
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:post_category_id, :title, :text, :image)
+  end
+
+
+
+    
 
 
 
