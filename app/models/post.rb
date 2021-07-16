cc61c3c5b2fa699dbe7f627d77324aac7715b415
Shoya-cyber <shoyasan0627@gmail.com>
2021-07-16 23:27:53 +0900

    ActiveStorageのアソシエーションを修正

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,6 @@ class Post < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :post_category
 
-  has_many_attached :image
+  has_one_attached :image
 
 end
