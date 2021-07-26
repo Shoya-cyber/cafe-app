2a98590be226c5d53801376525821c8581a85729
Shoya-cyber <shoyasan0627@gmail.com>
2021-07-26 10:40:25 +0900

    userモデルにバリデーションとアソシエーション記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,19 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :posts
+
+  with_options presence: true do
+    validates :name
+    validates :name_reading, format: { with: /\A[ァ-ヶー－]+\z/, message: '全角カタカナで入力してください'}
+    validates :email, uniqueness: true
+    validates :password
+    validates :phone_number, length: { maximum: 11}, format: { with: /\A\d{10,11}\z/, message: '(-)なし、11桁以内で入力してください'}
+    validates :birth_date
+  end
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: '半角の英字と数字を1文字以上含めてください'
+
 end
