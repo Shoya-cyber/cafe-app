00e59c38af11f9bf1fde38829b82ba568e01090c
Shoya-cyber <shoyasan0627@gmail.com>
2021-07-26 21:31:28 +0900

    userモデルのバリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,14 +8,16 @@ class User < ApplicationRecord
 
   with_options presence: true do
     validates :name
-    validates :name_reading, format: { with: /\A[ァ-ヶー－]+\z/, message: '全角カタカナで入力してください'}
+    validates :name_reading, format: { with: /\A[ァ-ヶー－]+\z/, message: 'is invalid. Input full-width characters.'}
     validates :email, uniqueness: true
     validates :password
-    validates :phone_number, length: { maximum: 11}, format: { with: /\A\d{10,11}\z/, message: '(-)なし、11桁以内で入力してください'}
+    validates :phone_number, length: { maximum: 11 }
     validates :birth_date
   end
 
+  validates :phone_number, format: { with: /\A\d{10,11}\z/, message: 'is invalid. Not include hyphen(-).'}
+
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates_format_of :password, with: PASSWORD_REGEX, message: '半角の英字と数字を1文字以上含めてください'
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'is invalid. Password Include both letters and numbers.'
 
 end
