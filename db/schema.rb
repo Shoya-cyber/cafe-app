a6b58faf3b9b3e2fda1c82d0c60ad7f54d53993b
Shoya-cyber <shoyasan0627@gmail.com>
2021-07-27 20:56:11 +0900

    postsマイグレーションファイル再作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_26_012612) do
+ActiveRecord::Schema.define(version: 2021_07_27_115154) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -37,8 +37,10 @@ ActiveRecord::Schema.define(version: 2021_07_26_012612) do
     t.integer "post_category_id", null: false
     t.string "title", null: false
     t.text "text", null: false
+    t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -58,4 +60,5 @@ ActiveRecord::Schema.define(version: 2021_07_26_012612) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "posts", "users"
 end
