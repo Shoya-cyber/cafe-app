ab4d5329d96da0d09506e978318b64e5634ed616
Shoya-cyber <shoyasan0627@gmail.com>
2021-07-29 21:24:01 +0900

    productモデルにアソシエーションとバリデーション設定

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,10 @@
 class Product < ApplicationRecord
+  has_one_attached :image
+
+  with_options presence: true do 
+    validates :product_name
+    validates :info, length: { maximum: 50}
+    validates :price, format: { with: /\A[0-9]+\z/, massage: 'is invalid. Input half-width characters.' }
+    validates :image
+  end
 end
