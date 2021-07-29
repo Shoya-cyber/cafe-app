9b4eb42820dbee60608428f1f0f2d99212b8e877
Shoya-cyber <shoyasan0627@gmail.com>
2021-07-29 20:04:41 +0900

    ルーティングにproducts/index設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get 'products/index'
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
   root to: "homes#index"
   resources :posts
+  resources :products, only: [:index]
 
 end
