7d4dc8884fbc62943c78b13a7e41c033e2dbf5c8
Shoya-cyber <shoyasan0627@gmail.com>
2021-07-30 18:22:33 +0900

    controllerにedit,updateアクション追加

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -18,6 +18,13 @@ class ProductsController < ApplicationController
     end
   end
 
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+  end
+
   private
 
   def product_params
