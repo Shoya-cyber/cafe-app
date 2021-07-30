6181dcb5c62f8ccb4b83eb3072d676b86f2a11f2
Shoya-cyber <shoyasan0627@gmail.com>
2021-07-30 15:39:27 +0900

    createアクションにインスタンス変数を定義

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,5 +7,17 @@ class ProductsController < ApplicationController
   end
 
   def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to products_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:product_name, :info, :price, :image)
   end
 end
