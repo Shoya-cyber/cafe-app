ab2c5cd3ae3f0a82f10086996fb835caba3a8555
Shoya-cyber <shoyasan0627@gmail.com>
2021-08-06 03:38:30 +0900

    order controllerにcustomer_infoアクション設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,5 @@
 class OrdersController < ApplicationController
+
+  def customer_info
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,6 @@ Rails.application.routes.draw do
   post 'carts/add_cart', to: 'carts#add_cart'
   patch 'carts/change_quantity', to: 'carts#change_quantity', as: 'change_item_quantity'
   delete 'carts/destroy_carts_item', to: 'carts#destroy_carts_item', as: 'destroy_carts_item'
+
+  get 'orders/customer_info', to: 'orders#customer_info', as: 'customer_info'
 end
