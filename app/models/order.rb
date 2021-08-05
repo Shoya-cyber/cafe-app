473e814e6b77fe8f50e7d83b34a1c8440a439f09
Shoya-cyber <shoyasan0627@gmail.com>
2021-08-06 02:29:42 +0900

    order_detailモデルのアソシエーション、バリデーション設定

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,8 @@
 class Order < ApplicationRecord
 
   belongs_to :user
+  has_many :products, through: :order_details
+  has_many :order_details, dependent: :destroy
 
   with_options presence: true do
     validates :total_quantity
