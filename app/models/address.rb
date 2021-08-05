cf07643c475f6b74586a9881ff05e39fe7fa2c63
Shoya-cyber <shoyasan0627@gmail.com>
2021-08-06 02:14:59 +0900

    addressモデルのバリデーション設定

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,10 @@
 class Address < ApplicationRecord
   belongs_to :user
+
+  with_options presence: true do
+    validates :postal_code, format: { with: /\A\d{7}\z/, message: "is invalid. Not include hyphen(-)." }
+    validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank." }
+    validates :city
+    validates :house_number
+  end
 end
