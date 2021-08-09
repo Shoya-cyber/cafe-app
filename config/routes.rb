3ca262418ebf1070fad28ff1a73d1b5b3061637f
Shoya-cyber <shoyasan0627@gmail.com>
2021-08-10 01:29:49 +0900

    ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,5 @@ Rails.application.routes.draw do
   patch 'carts/change_quantity', to: 'carts#change_quantity', as: 'change_item_quantity'
   delete 'carts/destroy_carts_item', to: 'carts#destroy_carts_item', as: 'destroy_carts_item'
 
-  get 'orders/customer_info', to: 'orders#customer_info', as: 'customer_info'
+  get 'card_address/new', to: 'card_addresses#new', as: 'new_card_address'
 end
