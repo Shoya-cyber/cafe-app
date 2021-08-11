1b6ba31553574610bd12edcf4ed5f77764ca2ba5
Shoya-cyber <shoyasan0627@gmail.com>
2021-08-12 03:24:17 +0900

    userモデル記述修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
 
   has_many :posts
   has_one :card, dependent: :destroy
-  has_one :adress, dependent: :destroy
+  has_one :address, dependent: :destroy
   has_many :orders, dependent: :nullify
 
   with_options presence: true do
