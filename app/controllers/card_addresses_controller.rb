37677f828066ba0c5af86e75b33fdd460e555287
Shoya-cyber <shoyasan0627@gmail.com>
2021-08-12 03:04:24 +0900

    card_address controllerのcreateアクション修正

--- app/controllers/card_addresses_controller.rb
+++ app/controllers/card_addresses_controller.rb
@@ -5,9 +5,10 @@ class CardAddressesController < ApplicationController
 
   def create
     @card_address = CardAddress.new(card_address_params)
+
     if @card_address.valid?
       @card_address.save
-      rendirect_to new_order_path
+      redirect_to new_order_path
     else
       render :new
     end
@@ -15,18 +16,17 @@ class CardAddressesController < ApplicationController
 
   private
 
+  def card_params
+    params.require(:card_address).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name).merge(card_id: params[:card_id], user_id: current_user.id)
+  end
+
   def card_address_params
-    params.require(:card_address).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name).merge(token: params[:token], user_id: current_user.id, customer_id: customer.id)
-  end
-
-  def card_create
     Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
-    customer = Payjp::Customer.create(description: 'test', card: params[:token])
-
-    @card = Card.new(
-      customer_id = customer.id,
-      card_id = params[:token],
-      user_id = current_user.id
-    )
+    customer = Payjp::Customer.create(description: 'test', card: card_params[:card_id])
+    params.require(:card_address).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name).merge(
+      card_id: params[:card_id], 
+      user_id: current_user.id, 
+      customer_id: customer.id)
   end
+
 end
