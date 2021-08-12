e54e97d8b79f1d8e4394d74f2b87b988a8164564
Shoya-cyber <shoyasan0627@gmail.com>
2021-08-13 07:52:07 +0900

    ユーザー詳細ページレイアウト作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,12 @@
 class UsersController < ApplicationController
 
   def show
+    @user = User.find(params[:id])
     
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    card = Card.find_by(user_id: @user.id)
+    customer= Payjp::Customer.retrieve(card.customer_id)
+    @card = customer.cards.first
+
   end
 end
