3609f735a355bfa92fd24d7c5c493e0cb67ddffb
Shoya-cyber <shoyasan0627@gmail.com>
2021-08-12 14:14:52 +0900

    orderのcreateアクション記述

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -21,6 +21,43 @@ class OrdersController < ApplicationController
                    quantity: cart["quantity"].to_i,
                    sub_total: sub_total })
     end
-    
   end
+    
+  def create
+    session[:cart].each do |cart|
+      total_quantity = 0
+      total_price = 0
+
+      total_quantity += cart["quantity"].to_i
+      total_price += cart["price"].to_i 
+        return
+        
+        @order = Order.new(
+          user_id: current_user.id,
+          address_id: current_user.address.id,
+          card_id: current_user.card.id,
+          total_quantity: total_quantity,
+          total_price: total_price
+        )
+        
+        @order_detail = OrderDetail.new(
+          order_id: current_order.id,
+          product_id: cart["product_id"],
+          quantity: cart["quantity"]
+        )
+    end
+    if @order.valid? && @order_detail.valid?
+      @order.save && @order_detail.save
+      session[:cart].clear
+      redirect_to order_comfirm_path
+    else
+      render :new
+    end
+
+
+  end
+
+  private
+
+  
 end
