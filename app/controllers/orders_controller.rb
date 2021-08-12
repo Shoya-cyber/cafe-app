70006c57615cebe2505d3143c9f0e6b1b08f690d
Shoya-cyber <shoyasan0627@gmail.com>
2021-08-12 09:04:35 +0900

    購入確認ページのレイアウト作成

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,26 @@
 class OrdersController < ApplicationController
   def new
     @order = Order.new
+    @address = Address.find_by(user_id: current_user.id)
+
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    card = Card.find_by(user_id: current_user.id)
+    customer= Payjp::Customer.retrieve(card.customer_id)
+    @card = customer.cards.first
+
+    @cart = []
+    session[:cart].each do |cart|
+      product = Product.find_by(id: cart["product_id"])
+      sub_total = product.price * cart["quantity"].to_i
+      next unless product
+
+      @cart.push({ product_id: product.id,
+                   image: product.image,
+                   name: product.product_name,
+                   price: product.price,
+                   quantity: cart["quantity"].to_i,
+                   sub_total: sub_total })
+    end
+    
   end
 end
