5ee389c8e6a0161ac0957a5e5a5ad47d64bdbf48
Shoya-cyber <shoyasan0627@gmail.com>
2021-08-13 08:46:19 +0900

    navi barのマイページにリンク設定、card_address情報がなくても遷移できるように設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,11 +2,19 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    
-    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
-    card = Card.find_by(user_id: @user.id)
-    customer= Payjp::Customer.retrieve(card.customer_id)
-    @card = customer.cards.first
+
+    if @user.address.present?
+      Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+      card = Card.find_by(user_id: @user.id)
+      customer= Payjp::Customer.retrieve(card.customer_id)
+      @card = customer.cards.first
+    end
+
+  end
+
+  private
+
+  def card
 
   end
 end
