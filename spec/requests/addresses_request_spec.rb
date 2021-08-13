084c6c8414cc8b91b051865bbd0e420b3a91b3df
Shoya-cyber <shoyasan0627@gmail.com>
2021-08-13 12:22:38 +0900

    address controllerインストール

---
+++ spec/requests/addresses_request_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe "Addresses", type: :request do
+
+  describe "GET /edit" do
+    it "returns http success" do
+      get "/addresses/edit"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /update" do
+    it "returns http success" do
+      get "/addresses/update"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+end
